69bbb7f0b34076a414821c483463cd98676c8453
zvikasp <89988171+zvikasp@users.noreply.github.com>
2022-05-31 21:08:15 +0530

    Add HelloWorld SpringBoot App

---
+++ HelloSimplilearnAWS/src/test/java/com/deploy/aws/HelloSimplilearnAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSimplilearnAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
